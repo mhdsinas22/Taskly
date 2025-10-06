086e5c4d50f4ac67c41509c1c9d80062b4f8cf01
mhdsinas22 <sinasmuhammed2@gmail.com>
2025-10-06 22:06:11 +0530

    task_state finsed

---
+++ lib/bloc/task_bloc/task_state.dart
@@ -0,0 +1,25 @@
+import 'package:borading_week2/models/task.dart';
+import 'package:equatable/equatable.dart';
+
+abstract class TaskState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class TaskInitial extends TaskState {}
+
+class TaskLoading extends TaskState {}
+
+class TaskLoaded extends TaskState {
+  final List<TaskModel> tasks;
+  TaskLoaded(this.tasks);
+  @override
+  List<Object?> get props => [tasks];
+}
+
+class TaskError extends TaskState {
+  final String message;
+  TaskError(this.message);
+  @override
+  List<Object?> get props => [message];
+}
