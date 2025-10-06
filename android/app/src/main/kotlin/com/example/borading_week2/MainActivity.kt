570a1c6a444f75190b4381f429ad6c0f1b3a05aa
mhdsinas22 <sinasmuhammed2@gmail.com>
2025-10-06 11:11:02 +0530

    starting point

---
+++ android/app/src/main/kotlin/com/example/borading_week2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.borading_week2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:borading_week2/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
